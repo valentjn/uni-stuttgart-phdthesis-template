3f514017acb93331c5f1ba29a22febed4a747d08
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2018-08-20 08:05:49 +0200

    Make lua/progressTable work with -- in the table

--- lua/progressTable.lua
+++ lua/progressTable.lua
@@ -3,7 +3,7 @@ function generateTotalRow()
   sum = {}
   nRows = 0
   
-  for i = 0, 13 do
+  for i = 0, 18 do
     sum[i] = 0
   end
   
@@ -18,27 +18,35 @@ function generateTotalRow()
       for s in line:gmatch("[^&]+") do
         if (i ~= 0) and (i ~= 3) and (i ~= 6) and (i ~= 13) then
           weighted = false
+          ignore = false
           
           if i == 12 then
             s = s:gsub("\\\\.*", ""):gsub("\\tfoot", "")
             s = s:gsub("\\pr{", ""):gsub("}", "")
             weighted = true
           elseif (i >= 7) and (i <= 11) then
-            s = s:gsub("\\yes", "1"):gsub("\\no", "0")
-            weighted = true
+            if s:find("%-%-") then
+              ignore = true
+            else
+              s = s:gsub("\\yes", "1"):gsub("\\no", "0")
+              weighted = true
+              sum[i+7] = sum[i+7] + pageCount
+            end
           end
           
-          summand = tonumber(s)
-          
-          if i == 2 then
-            pageCount = summand
+          if not ignore then
+            summand = tonumber(s)
+            
+            if i == 2 then
+              pageCount = summand
+            end
+            
+            if weighted then
+              summand = pageCount * summand
+            end
+            
+            sum[i] = sum[i] + summand
           end
-          
-          if weighted then
-            summand = pageCount * summand
-          end
-          
-          sum[i] = sum[i] + summand
         end
         
         i = i + 1
@@ -56,8 +64,8 @@ $\sum$ &
 \pr{%.0f}]],
       sum[1], sum[2], 100*sum[1]/sum[2],
       sum[4], sum[5], 100*sum[4]/sum[5],
-      100*sum[7] /sum[2], 100*sum[8] /sum[2], 100*sum[9] /sum[2],
-      100*sum[10]/sum[2], 100*sum[11]/sum[2],
+      100*sum[7] /sum[14], 100*sum[8] /sum[15], 100*sum[9] /sum[16],
+      100*sum[10]/sum[17], 100*sum[11]/sum[18],
       sum[12]/sum[2])
   output = output:gsub("\n", " ")
   
